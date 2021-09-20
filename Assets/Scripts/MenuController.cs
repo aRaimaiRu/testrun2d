fd5747b640cbcee700bafea7c6d81de0c46de1ee
= <=>
2021-09-20 15:02:26 +0700

    add GameMenu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
-    public GameObject PauseMenu;
+    public GameObject PauseMenu; 
     public GameObject DeathMenu;
     public GameObject WinMenu;
     public GameObject GameplayUI;
     // Start is called before the first frame update
-    public void Pause(){
-        Time.timeScale = 0f;
-        GameplayUI.SetActive(false);
-        PauseMenu.SetActive(true);
-    }
     public void Resume(){
         Time.timeScale = 1f;
-        GameplayUI.SetActive(true);
         PauseMenu.SetActive(false);
+        GameplayUI.SetActive(true);
+    }
+    public void Pause(){
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+        GameplayUI.SetActive(false);
     }
     public void Restart(){
         Time.timeScale = 1f;
@@ -26,12 +26,10 @@ public class MenuController : MonoBehaviour
     public void Death(){
         Time.timeScale = 0f;
         DeathMenu.SetActive(true);
-        PauseMenu.SetActive(false);
         GameplayUI.SetActive(false);
     }
     public void Win(){
         WinMenu.SetActive(true);
-        PauseMenu.SetActive(false);
         GameplayUI.SetActive(false);
     }
 }
