6d257b5a3d2c867fce9f77f959a18dead6974313
Pattrawut1 <87476142+Pattrawut1@users.noreply.github.com>
2021-09-28 10:48:14 +0700

    Aot

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,17 +5,36 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public GameObject WinMenu;
+    public GameObject DeathMenu;
     public GameObject GameplayUI;
+    public GameObject PauseMenu;
     // Start is called before the first frame update
     public void Win(){
         WinMenu.SetActive(true);
         GameplayUI.SetActive(false);
     }
     public void Death(){
+        Time.timeScale = 0f;
+        DeathMenu.SetActive(true);
+        PauseMenu.SetActive(false);
         WinMenu.SetActive(false);
         GameplayUI.SetActive(false);
         StartCoroutine("Wait");
     }
+    public void Restart(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Pause(){
+        Time.timeScale = 0f;
+        GameplayUI.SetActive(false);
+        PauseMenu.SetActive(true);
+}
+public void Resume(){
+        Time.timeScale = 1f;
+        GameplayUI.SetActive(true);
+        PauseMenu.SetActive(false);
+}
     IEnumerator Wait(){
         yield return new WaitForSeconds(0.3f);
         Time.timeScale = 0f;
